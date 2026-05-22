f34ef342b5aec1211d5edaf32e289b2cd4adf636
MALAIRAJ23 <malai2326raj@gmail.com>
2026-05-22 22:20:41 +0530

    first

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /home/madhan/vian-luxure/src/app/layout.tsx
+// File: E:\vianluxure\src\app\layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
